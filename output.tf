563b4bb94130c66cc5014bf20f2849eb3671cc2e
Slav Donchev <slav.donchev@gmail.com>
2020-11-09 16:04:34 +0200

    make ssh_private_key conditional on TLS key pair

--- output.tf
+++ output.tf
@@ -4,6 +4,6 @@ output "vm_public_ip" {
 }
 
 output "ssh_private_key" {
-  description = "The private key to access the VM."
-  value       = tls_private_key.tfca[0].private_key_pem
+  description = "The private key to access the VM. Populated only if the key was created via Terraform."
+  value       = local.ssh_key_pair_create ? tls_private_key.tfca[0].private_key_pem : ""
 }
