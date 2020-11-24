06679643b7603faee5d3790fac5e17e9b7af7b7b
Slav Donchev <slav.donchev@gmail.com>
2020-11-24 11:05:52 +0200

    add test fixture

---
+++ test/fixtures/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.30"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0"
+    }
+  }
+}
