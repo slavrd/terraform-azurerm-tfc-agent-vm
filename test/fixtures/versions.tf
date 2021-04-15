7cb5287c31f53efe52d09473e28eb6d115060f23
Slav Donchev <slav.donchev@gmail.com>
2021-04-15 16:58:39 +0300

    relax version constraints

--- test/fixtures/versions.tf
+++ test/fixtures/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.30"
+      version = ">= 2.30"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
   }
 }
